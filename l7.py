3075efb841134f2644180f42e85795adf852c5cf
GithubRedMouth <90645738+GithubRedMouth@users.noreply.github.com>
2022-11-30 01:43:56 +0300

    первый комит

---
+++ l7.py
@@ -0,0 +1,18 @@
+# типы данных str() int() float() bool()
+# Bool может быть True или False
+r = True  # правда
+f = False  # ложь
+
+#Тип данных string
+w='tool'
+
+#пустота
+w2=123
+w2=None# выведит None потому что выполняется линейно
+
+
+print(type(r))
+print(type(f))
+print(w)
+print(type(w2))
+
